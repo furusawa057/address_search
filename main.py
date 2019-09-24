38395b6d087f9ac27aaa1299b4ddbdf681135300
furusawa057 <sfurusawa057@gmail.com>
2019-09-24 11:39:11 +0900

    任意の郵便番号を入力して検索できるようにした

--- main.py
+++ main.py
@@ -2,19 +2,18 @@ import requests
 
 
 def main():
-    # 1600023->'東京都新宿区西新宿'
     # 入力
-    zipcode = '1600023'
+    zipcode = input('郵便番号を入力してください:')
+
     url = f'http://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}'
 
     # 計算
-    # リクエスト送る->受け取ってParse(->フォーマット)
     response = requests.get(url)
 
     # 出力
-
     json = response.json()
     address = json['results'][0]
+
     # for n in range(1, 4):
     #     print(json['results'][0][f'address{n}'])  <-何の情報を print してるのか初見ではわかりにくい！！！！
 
